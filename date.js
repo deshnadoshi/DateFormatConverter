8c9e595478fd161aa49ba6b85e8216801dd6f3d3
deshnadoshi <deshna.doshi@gmail.com>
2024-01-22 14:52:02 -0500

    Command line read

--- date.js
+++ date.js
@@ -1,3 +1,13 @@
-function date_convert (month, day, year, hour, minute){
+const readline = require('node:readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  date_input = "";  
 
-}
+  readline.question(`Enter a date of the format YYYYMMDDTHHMMSS`, full_date => {
+    date_input = full_date; 
+    readline.close();
+  });
+
+  console.log(date_input); 
